5763ab56ce3f1ba4055f3e4eb96bbd32cd176335
kainatalicoding <159925107+KainatAlicoding@users.noreply.github.com>
2025-12-09 19:39:04 +0500

    Update production URL in Docusaurus config

--- frontend/docusaurus.config.ts
+++ frontend/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://hackathon1-textbook.vercel.app/',
+  url: 'hackathon1-textbook-8cqs.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
