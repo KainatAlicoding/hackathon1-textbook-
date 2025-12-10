c9ee840848c2069e4d564ee4d5fc2e92e325bdc6
kainat ali <akainat776@gmail.com>
2025-12-10 21:25:13 +0500

    Updated the docusaurus.config.ts file for github pages

--- frontend/docusaurus.config.ts
+++ frontend/docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://github.com/KainatAlicoding',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: 'https://github.com/KainatAlicoding/hackathon1-textbook-',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'https://github.com/KainatAlicoding', // Usually your GitHub org/user name.
+  projectName: 'https://github.com/KainatAlicoding/hackathon1-textbook-', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
